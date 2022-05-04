7fe7c0fec8a1e14726da0986cd66a12829b34b79
tylerengland25 <tyler_england35@yahoo.com>
2022-05-04 16:43:08 -0700

    56% retrying analysis

--- backend/preprocess/preprocess.py
+++ backend/preprocess/preprocess.py
@@ -25,34 +25,20 @@ def load_pitching_totals():
     return pitching_totals
 
 
-def load_pitching_details(type):
-    # Load pitching details
-    pitching_details = pd.read_csv('backend/data/pitching/details.csv')
-    pitching_details['date'] = pd.to_datetime(pitching_details['date'])
-    pitching_details['cwpa'] = pitching_details['cwpa'].apply(lambda x: float(x.strip('%')))
- 
-    if type =='pitching_starters':
-        # Starting pitchers
-        df = pitching_details[
-            pitching_details['ip'] >= 6
-        ].groupby(
-            ['date', 'visitor', 'home', 'team']
-        ).sum(
-        ).reset_index()
-        return df
-
-    elif type == 'pitching_bullpen':
-        # Bullpen pitchers
-        df = pitching_details[
-            pitching_details['ip'] < 6
-        ].groupby(
-            ['date', 'visitor', 'home', 'team']
-        ).sum(
-        ).reset_index()
-        return df
-
-    return None
+def load_starters():
+    df = pd.read_csv('backend/data/pitching/starters.csv')
+    df['date'] = pd.to_datetime(df['date'])
+    df['cwpa'] = df['cwpa'].apply(lambda x: float(x.strip('%')))
     
+    return df
+
+
+def load_bullpen():
+    df = pd.read_csv('backend/data/pitching/bullpen.csv')
+    df['date'] = pd.to_datetime(df['date'])
+    df['cwpa'] = df['cwpa'].apply(lambda x: float(x.strip('%')))
+    
+    return df
 
 
 def load_schedules():
@@ -76,10 +62,105 @@ def feature_engineer(df, type):
         df['1b'] = df['h'] - (df['2b'] + df['3b'] + df['hr'])
         df['slg'] = (df['1b'] + 2*df['2b'] + 3*df['3b'] + 4*df['hr']) / df['ab']
         df['ops'] = df['obp'] + df['slg']
-    elif type == 'pitching_totals' or type == 'pitching_starters' or type == 'pitching_visitor':
+    elif type == 'pitching_totals':
         df['era'] = 9 * df['er'] / df['ip']
+    elif type == 'pitching_details':
+        df['era_bullpen'] = 9 * df['er_bullpen'] / df['ip_bullpen']
+        df['era_starters'] = 9 * df['era_starters'] / df['era_starters']
 
     return df
+
+
+
+def sma(bin, df, type, schedule):
+
+    sma_df = pd.DataFrame()
+    
+    for team in df['team'].unique():
+        for season in df['date'].dt.year.unique():
+            team_df = df[(df['team'] == team) & (df['date'].dt.year == season)].sort_values(by=['date']).copy()
+
+            team_schedule = schedule[
+                ((schedule['home'] == team) | (schedule['visitor'] == team)) & 
+                (schedule['date'].dt.year == season)
+            ].drop(['season'], axis=1)
+
+            team_df = pd.merge(
+                team_schedule,
+                team_df,
+                left_on=['date', 'visitor', 'home'],
+                right_on=['date', 'visitor', 'home'],
+                how='left'
+            ).drop_duplicates(
+                ['date', 'visitor', 'home', 'team']
+            )
+
+            for col in list(set(df.columns).difference({'date', 'visitor', 'home', 'team', 'player'})):
+                team_df[col] = team_df[col].rolling(bin, closed='left').mean()
+            
+            team_df['team'] = team_df['team'].shift(1)
+            sma_df = sma_df.append(team_df.dropna(axis=0), ignore_index=True)
+    
+    sma_df = pd.merge(
+        sma_df, 
+        df[['date', 'visitor', 'home', 'team', 'h']],
+        left_on=['date', 'visitor', 'home', 'team'], 
+        right_on=['date', 'visitor', 'home', 'team'],
+        suffixes=('', '_target'), 
+        how='left'
+    ).drop_duplicates(
+        ['date', 'visitor', 'home', 'team']
+    )
+
+    sma_df = feature_engineer(sma_df, type)
+
+    return sma_df.sort_values(by=['date'])
+
+
+def cma(df, schedule, type):
+
+    cma_df = pd.DataFrame()
+
+    for team in df['team'].unique():
+        for season in df['date'].dt.year.unique():
+            team_df = df[(df['team'] == team) & (df['date'].dt.year == season)].sort_values(by=['date']).copy()
+            
+            team_schedule = schedule[
+                ((schedule['home'] == team) | (schedule['visitor'] == team)) & 
+                (schedule['date'].dt.year == season)
+            ].drop(['season'], axis=1)
+
+            team_df = pd.merge(
+                team_schedule,
+                team_df,
+                left_on=['date', 'visitor', 'home'],
+                right_on=['date', 'visitor', 'home'],
+                how='left'
+            ).drop_duplicates(
+                ['date', 'visitor', 'home', 'team']
+            )
+            
+            team_df['team'] = team_df['team'].shift(1)
+            for col in list(set(team_df.columns).difference({'date', 'visitor', 'home', 'team', 'player'})):
+                team_df[col] = team_df[col].expanding().mean()
+                team_df[col] = team_df[col].shift(1)
+            
+            cma_df = cma_df.append(team_df.dropna(axis=0), ignore_index=True)
+    
+    cma_df = pd.merge(
+        cma_df, 
+        df[['date', 'visitor', 'home', 'team', 'h']],
+        left_on=['date', 'visitor', 'home', 'team'], 
+        right_on=['date', 'visitor', 'home', 'team'],
+        suffixes=('', '_target'),
+        how='left'
+    ).drop_duplicates(
+        ['date', 'visitor', 'home', 'team']
+    )
+
+    cma_df = feature_engineer(cma_df, type)
+
+    return cma_df.sort_values(by=['date'])
 
 
 def ema(df, type, schedule):
@@ -131,23 +212,84 @@ def ema(df, type, schedule):
 def preprocess(type):
     if type == 'batting':
         df = load_batting_totals()
-    elif type == 'pitching_totals':
-        df = load_pitching_totals()
-    elif type == 'pitching_starters':
-        df = load_pitching_details('pitching_starters')
-    elif type == 'pitching_bullpen':
-        df = load_pitching_details('pitching_bullpen')
-    
+        schedule = load_schedules()
+        sma_df = sma(3, df, type, schedule)
+        cma_df = cma(df, schedule, type)
+        ema_df = ema(df, type, schedule)
 
-    schedule = load_schedules()
-    ema_df = ema(df, type, schedule)
-        
-    return ema_df[['date', 'visitor', 'home', 'team', 'h', 'h_target']]
+        df = pd.merge(
+            sma_df[['date', 'visitor', 'home', 'team', 'h', 'obp', 'slg', 'ops', 'ba']],
+            cma_df[['date', 'visitor', 'home', 'team', 'h', 'obp', 'slg', 'ops', 'ba']],
+            left_on=['date', 'visitor', 'home', 'team'], 
+            right_on=['date', 'visitor', 'home', 'team'],
+            suffixes=('_sma', '_cma')
+        )
+
+        df = pd.merge(
+            df,
+            ema_df[['date', 'visitor', 'home', 'team', 'h', 'obp', 'slg', 'ops', 'ba', 'h_target']],
+            left_on=['date', 'visitor', 'home', 'team'], 
+            right_on=['date', 'visitor', 'home', 'team'],
+            suffixes=('', '_ema')
+        )
+
+        return df
+
+    elif type.split('_')[0] == 'pitching':
+        if type.split('_')[-1] == 'totals':
+            df = load_pitching_totals()
+
+            cols = ['date', 'visitor', 'home', 'team', 'h', 'era', 'er']
+
+        elif type.split('_')[-1] == 'details':
+            starters = load_starters()
+            bullpen = load_bullpen()
+
+            df = pd.merge(
+                bullpen,
+                starters,
+                left_on=['date', 'visitor', 'home', 'team'], 
+                right_on=['date', 'visitor', 'home', 'team'],
+                suffixes=('_bullpen', '_starters'),
+                how='left'
+            )
+            df = df.fillna(0)
+            df['h'] = df['h_bullpen'] + df['h_starters']
+
+            cols = [
+                'date', 'visitor', 'home', 'team', 
+                'h_bullpen', 'era_bullpen', 'er_bullpen',
+                'h_starters', 'era_starters', 'er_starters'
+            ]
+
+
+        schedule = load_schedules()
+        sma_df = sma(3, df, type, schedule)
+        cma_df = cma(df, schedule, type)
+        ema_df = ema(df, type, schedule)
+
+        df = pd.merge(
+            sma_df[cols],
+            cma_df[cols],
+            left_on=['date', 'visitor', 'home', 'team'], 
+            right_on=['date', 'visitor', 'home', 'team'],
+            suffixes=('_sma', '_cma')
+        )
+
+        df = pd.merge(
+            df,
+            ema_df[cols],
+            left_on=['date', 'visitor', 'home', 'team'], 
+            right_on=['date', 'visitor', 'home', 'team'],
+            suffixes=('', '_ema')
+        )
+
+        return df
 
 
 def load_preprocessed_data():
     batting_df = preprocess('batting')
-    pitching_df = preprocess('pitching_totals')
+    pitching_df = preprocess('pitching_details')
 
     df = pd.merge(
         batting_df,
@@ -170,16 +312,15 @@ def load_preprocessed_data():
     )
 
     df['most_hits'] = np.where(
-        df['h_target_batting_home'] > df['h_target_batting_visitor'], 1, 0
+        df['h_target_home'] > df['h_target_visitor'], 1, 0
     )
 
-    df['total_hits'] = df['h_target_batting_home'] + df['h_target_batting_visitor']
+    df['total_hits'] = df['h_target_home'] + df['h_target_visitor']
 
     df = df.drop(
         [
             'team_home', 'team_visitor', 
-            'h_target_batting_home', 'h_target_batting_visitor', 
-            'h_target_pitching_home', 'h_target_pitching_visitor'
+            'h_target_home', 'h_target_visitor'
         ], 
         axis=1
     )
